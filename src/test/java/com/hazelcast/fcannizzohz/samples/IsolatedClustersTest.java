b7d7072647dfff2a042f514b31a29a4768f0263d
Fabrizio Cannizzo <fabrizio.cannizzo@hazelcast.com>
2025-06-25 14:45:15 +0100

    added doc and better testing

--- src/test/java/com/hazelcast/fcannizzohz/samples/IsolatedClustersTest.java
+++ src/test/java/com/hazelcast/fcannizzohz/samples/IsolatedClustersTest.java
@@ -4,17 +4,23 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.test.TestHazelcastFactory;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.EntryProcessor;
 import com.hazelcast.map.IMap;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Map;
 
 import static com.hazelcast.test.HazelcastTestSupport.assertClusterSizeEventually;
 import static com.hazelcast.test.HazelcastTestSupport.assertEqualsEventually;
 import static com.hazelcast.test.HazelcastTestSupport.randomName;
 
 /**
- * This test shows how I can run multiple tests in parallel and in isolation, by assigning random names to the
+ * This test shows how I can run multiple tests in parallel and in isolation, to test
+ * business logic on Hazelcast data structures.
+ * By running tests in parallel I can expedite their execution.
+ * Isolation can be achieved by by assigning random names to the
  * cluster so each test doesn't interfere with the other.
  */
 @RunWith(HazelcastParallelClassRunner.class)
@@ -30,12 +36,15 @@ public class IsolatedClustersTest {
         HazelcastInstance[] members = factory.newInstances(serverConfig, 2);
         HazelcastInstance client = factory.newHazelcastClient(clientConfig);
 
-        IMap<String, String> map = client.getMap("isolatedMap");
-        map.put("key", "valueA");
+        // Custom business logic
+        IMap<String, Integer> map = client.getMap("isolatedMap");
+        map.put("key", 1);
+        map.executeOnKey("key", (EntryProcessor<String, Integer, Integer>)
+                entry -> entry.setValue(entry.getValue() + 1));
 
         // verify cluster formed and data is available
         assertClusterSizeEventually(2, members[0]);
-        assertEqualsEventually(() -> map.get("key"), "valueA");
+        assertEqualsEventually(() -> map.get("key"), 2);
 
         factory.shutdownAll();
     }
@@ -50,12 +59,15 @@ public class IsolatedClustersTest {
         HazelcastInstance[] members = factory.newInstances(serverConfig, 2);
         HazelcastInstance client = factory.newHazelcastClient(clientConfig);
 
-        IMap<String, String> map = client.getMap("isolatedMap");
-        map.put("key", "valueB");
+        // Custom business logic
+        IMap<String, Integer> map = client.getMap("isolatedMap");
+        map.put("key", 1);
+        map.executeOnKey("key", (EntryProcessor<String, Integer, Integer>)
+                entry -> entry.setValue(entry.getValue() - 1));
 
-        // verify cluster formed and data is available
+        // verify cluster formed and data is processed
         assertClusterSizeEventually(2, members[0]);
-        assertEqualsEventually(() -> map.get("key"), "valueB");
+        assertEqualsEventually(() -> map.get("key"), 0);
 
         factory.shutdownAll();
 
